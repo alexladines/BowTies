c12ba1b45df7e399d0e1dcb2d054b0500ac03e52
alexladines <alexladines@outlook.com>
2019-05-11 14:38:15 -0700

    Create starter project files + add README.

---
+++ Bow Ties/View Controllers/BowTiesViewController.swift
@@ -0,0 +1,38 @@
+//  BowTiesViewController.swift
+//  Bow Ties
+//
+//  Created by Alex Ladines on 5/10/19.
+//  Copyright © 2019 Alex Ladines. All rights reserved.
+//
+
+import UIKit
+
+class BowTiesViewController: UIViewController {
+
+  // MARK: - IBOutlets
+  @IBOutlet weak var segmentedControl: UISegmentedControl!
+  @IBOutlet weak var imageView: UIImageView!
+  @IBOutlet weak var nameLabel: UILabel!
+  @IBOutlet weak var ratingLabel: UILabel!
+  @IBOutlet weak var timesWornLabel: UILabel!
+  @IBOutlet weak var lastWornLabel: UILabel!
+  @IBOutlet weak var favoriteLabel: UILabel!
+
+  // MARK: - View Life Cycle
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+
+  // MARK: - IBActions
+  @IBAction func segmentedControl(_ sender: Any) {
+
+  }
+
+  @IBAction func wear(_ sender: Any) {
+
+  }
+  
+  @IBAction func rate(_ sender: Any) {
+
+  }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# This is my second Core Data app.
+
+## The objectives for this app are:
+1) Understand the subclassing of NSManagedObject to make my own classes for each data entity to create 1 to 1 mapping between entities and classes in code.
+2) Learn about the data types available in Core Data entities including a few outside the usual string and number types.
+3) Learn about validating data to automatically check values before saving.
